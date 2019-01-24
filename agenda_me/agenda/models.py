31d2172b0875a2cc204caf9a19c4dba416aa80d5
noct <noct@debian.quarto.o>
2019-01-24 01:31:50 -0200

    :white_check_mark: [Pass] TDD - Test Create Agenda

--- agenda_me/agenda/models.py
+++ agenda_me/agenda/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Agenda(models.Model):    
+    titulo = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):        
+        return "{0}".format(self.titulo)
