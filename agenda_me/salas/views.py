b4b7f5b2b5a7f86026bd254722f7590068bc21fb
noct <noct@debian.quarto.o>
2019-01-23 22:58:32 -0200

    :green_check_mark: [Pass]TDD - Test Create Sala POST

--- agenda_me/salas/views.py
+++ agenda_me/salas/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import SalaSerializer
+from .models import Sala
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):   
+    queryset = Sala.objects.all()
+    serializer_class = SalaSerializer
+
+    def perform_create(self, serializer):
+        """Salva os dados do post e cria uma nova Sala."""
+        serializer.save()
