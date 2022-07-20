edd6cbd742248039977c462393e6feda60acd767
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-07-20 08:12:19 -0300

    feat: register Sala model on django-admin site

--- agenda_me/salas/admin.py
+++ agenda_me/salas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from salas.models import Sala
 
-# Register your models here.
+@admin.register(Sala)
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links =  ("id", "name")
