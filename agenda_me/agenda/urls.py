98a5739d5b0285086f8a028335a2bab49525328a
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-07-20 08:07:07 -0300

    refactor: django urlpatterns replaced from url to re_path/path

--- agenda_me/agenda/urls.py
+++ agenda_me/agenda/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, include
+from django.urls import re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView, DetailsView
 
 urlpatterns = {
-    url(r'^$', CreateView.as_view(), name="agenda"),    
-    url(r'^(?P<pk>[0-9]+)$',
+    re_path(r'^$', CreateView.as_view(), name="agenda"),    
+    re_path(r'^(?P<pk>[0-9]+)$',
         DetailsView.as_view(), name="details_genda"),      
 }
 urlpatterns = format_suffix_patterns(urlpatterns)
--- agenda_me/salas/urls.py
+++ agenda_me/salas/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url, include
+from django.urls import re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView, DetailsView
 from rest_framework import routers
 
 urlpatterns = {    
-    url(r'^$', CreateView.as_view(), name="create_sala"),
-    url(r'^(?P<pk>[0-9]+)$',
+    re_path(r'^$', CreateView.as_view(), name="create_sala"),
+    re_path(r'^(?P<pk>[0-9]+)$',
         DetailsView.as_view(), name="details_sala")
     
 }
