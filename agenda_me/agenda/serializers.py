1098da56d99588ca4441316f61362233f078c0c9
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-07-20 16:35:46 -0300

    feat: send auto-generated code to user (by email) on Agenda save
    automatically generate a security code, put the code in the mail body and store it in the database (in Agenda model table)

--- agenda_me/agenda/serializers.py
+++ agenda_me/agenda/serializers.py
@@ -1,24 +1,20 @@
 from rest_framework import serializers
 from .models import Agenda
 
-
 class AgendaSerializer(serializers.ModelSerializer):
     class Meta:        
         model = Agenda
-        fields = ('id', 'titulo','sala','date_init', 'date_end', 'date_created', 'date_modified', 'created_by', 'creator_email', 'creator_department')
-        read_only_fields = ('date_created', 'date_modified')
-    
-
+        fields = '__all__'
+        read_only_fields = ('date_created', 'date_modified', 'code')
 
     def create(self, validated_data):
-        try: 
+        try:
             return Agenda.objects.create(**validated_data)
         except Exception as e:
             error = {'message': ",".join(e.args) if len(e.args) > 0 else 'Unknown Error'}
             raise serializers.ValidationError(error, code=400)
 
-    def update(self, instance, validated_data):  
-       
+    def update(self, instance, validated_data):
         try:
             return super(AgendaSerializer, self).update(instance, validated_data)
         except Exception as e:
