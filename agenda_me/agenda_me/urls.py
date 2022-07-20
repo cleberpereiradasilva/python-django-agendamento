9dbd3a7ae91bc1e788a6642174f773067a55a145
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-07-20 08:18:36 -0300

    feat: routes for obtaining token and token_refresh
    'token/' and 'token/refresh/'

--- agenda_me/agenda_me/urls.py
+++ agenda_me/agenda_me/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sala/', include('salas.urls')),
-    path('agenda/', include('agenda.urls'))
+    path('agenda/', include('agenda.urls')),
    
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
