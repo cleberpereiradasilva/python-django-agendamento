0cba01d6b22b3cac8615868ec80b080f4584776f
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-07-21 16:35:50 -0300

    feat: block DELETE method at "/agenda/ID/" when "code" is not provided

--- agenda_me/agenda/views.py
+++ agenda_me/agenda/views.py
@@ -1,11 +1,10 @@
 from rest_framework import generics
 from .serializers import AgendaSerializer
-from rest_framework.views import APIView
 from .models import Agenda
 from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.exceptions import ParseError, NotFound, ValidationError
 from rest_framework import status
-
-from agenda_me.utils import MailSender
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -40,17 +39,39 @@ class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = Agenda.objects.all()
     serializer_class = AgendaSerializer
-    
-    # def partial_update(self, serializer):     
-    #     print('Atualizando..')   
-    #     try: 
-    #         serializer.save()             
-    #     except Exception as e: 
-    #         print('Erro?')           
-    #         error = {'message': ",".join(e.args) if len(e.args) > 0 else 'Unknown Error'}            
-    #         return Response(error,status=status.HTTP_400_BAD_REQUEST)
             
+    def delete(self, request: Request, *args, **kwargs):
+        """
+            Verifica se foi informado o código do agendamento, que foi gerado automaticamente ao ser criado.\n
+            Se o código estiver correto, exclui o item.\n
+            Se o código não for informado ou estiver incorreto, retorna 400 (Bad Request)
+        """
 
+        code = request.data.get('code', None)
+        agenda_id = kwargs.get('pk', None)
+
+        try:
+            agenda_instance: Agenda = Agenda.objects.get(id=agenda_id)
+        except Agenda.DoesNotExist:
+            raise NotFound()
+
+
+        if code is None:
+            raise ParseError(detail={
+                'detail': 'Informe o código do agendamento.',
+                'code': ['Este campo é obrigatório']
+            })
+
+        try:
+            if code == agenda_instance.code:
+                return super().delete(request, *args, **kwargs)
+            else:
+                raise ValueError
+        except ValueError:
+            raise ParseError(detail='Código do agendamento incorreto.')
+        except Exception as err:
+            print('Erro ao apagar agenda:', err)
+            raise ValidationError(detail={'detail': 'Erro interno no servidor.'}, code=status.HTTP_500_INTERNAL_SERVER_ERROR)
        
     
 
