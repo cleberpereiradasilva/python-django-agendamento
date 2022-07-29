3755ea06fe43af3fd43f616212cfa23c715e76b5
rafael <rafahreispc@gmail.com>
2022-07-29 14:55:31 -0300

    feat: added company field

--- agenda_me/salas/models.py
+++ agenda_me/salas/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Sala(models.Model):   
+    EMPRESA_CHOICES = [
+        ('Gimi', 'Gimi' ),
+        ('GPB', 'GPB'),
+        ('GBL', 'GBL'),
+        ('GS', 'GS')
+    ]
+
+
     id = models.AutoField(primary_key=True) 
-    name = models.CharField(max_length=255, blank=False, unique=True, verbose_name="Nome")
+    empresa = models.CharField(choices=EMPRESA_CHOICES, max_length=100, verbose_name="Empresa")
+    name = models.CharField(max_length=255, blank=False, verbose_name="Nome")
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
    
