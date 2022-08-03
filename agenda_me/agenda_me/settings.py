3c81b890ee4541d3d2f2e918e52e84a8963195eb
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-08-03 16:10:55 -0300

    style: sort INSTALLED_APPS by django, third-party and mine

--- agenda_me/agenda_me/settings.py
+++ agenda_me/agenda_me/settings.py
@@ -37,15 +37,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party apps
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'multiselectfield',
+
+    # my apps
     'salas',
     'agenda',
     'departamento',
