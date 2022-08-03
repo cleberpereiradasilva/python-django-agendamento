9e1a61c6db8fdc47c29bd474cc8152997b07a38b
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-08-03 17:13:26 -0300

    feat: implement new urls
    -agenda/periodic/
    -agenda/refresh_week/

--- agenda_me/agenda/urls.py
+++ agenda_me/agenda/urls.py
@@ -1,10 +1,11 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView
+from .views import CreateView, DetailsView, PeriodicAgendaViewSet, RefreshAgendaView
 
 urlpatterns = {
     re_path(r'^$', CreateView.as_view(), name="agenda"),    
-    re_path(r'^(?P<pk>[0-9]+)$',
-        DetailsView.as_view(), name="details_genda"),      
+    re_path(r'^(?P<pk>[0-9]+)$', DetailsView.as_view(), name="details_genda"),
+    path('periodic/', PeriodicAgendaViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'}), name="periodic_agenda"),
+    path('refresh_week/', RefreshAgendaView.as_view(), name="refresh_agenda")
 }
 urlpatterns = format_suffix_patterns(urlpatterns)
